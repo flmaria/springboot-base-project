959d26fc59c0821cecd80fd5011e215a7c631ce8
Flávio Luiz Maria <flaviolm@gmail.com>
2020-06-26 11:54:32 -0400

    Update ListUtils.java

--- src/main/java/com/flm/baseproject/utils/ListUtils.java
+++ src/main/java/com/flm/baseproject/utils/ListUtils.java
@@ -4,14 +4,14 @@ import java.util.List;
 
 public class ListUtils {
 
-	public static boolean isEmpty(List<? extends Object> list) {
+	public static boolean isEmpty(List<?> list) {
 		if (list == null || list.isEmpty())
 			return true;
 		
 		return false;
 	}
 	
-	public static boolean isNotEmpty(List<? extends Object> list) {
+	public static boolean isNotEmpty(List<?> list) {
 		return !isEmpty(list);
 	}
 	
